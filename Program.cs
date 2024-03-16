e1693338c176cd1562784176023219c26839cec9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:12:31 -0300

    Adicionando linha do codigo para primeira versão

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string tex = "a";
+Console.WriteLine("-----Tradutor de Baleês-----");
+Console.Write("digite a palavra que quer traduzir: ");
+tex = Console.ReadLine()!.ToLower();
+Console.WriteLine("Hum....\nVocê Digitou: "+tex);
+Console.WriteLine("Traduzindo ...");
+Thread.Sleep(1500);
+tex = tex.Replace("a","aaa")
+.Replace("e","eee")
+.Replace("i","iii")
+.Replace("o","ooo")
+.Replace("u","uuu")
+.Replace("A","AAA")
+.Replace("E","EEE")
+.Replace("I","III")
+.Replace("O","OOO")
+.Replace("U","UUU");;
+Console.WriteLine(tex);
